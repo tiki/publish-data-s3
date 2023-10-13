51972fd9daf198821d84cc7a56ebdb5cd6c075f2
Mike Audi <mike@mytiki.com>
2023-10-12 22:48:21 -0500

    fix: use event bridge events

--- src/handler.rs
+++ src/handler.rs
@@ -9,34 +9,33 @@ mod processor;
 mod s3;
 mod sqs;
 
-use aws_lambda_events::{event::sqs::SqsEventObj, s3::S3Event};
+use aws_lambda_events::{s3::S3Event, event::cloudwatch_events::CloudWatchEvent};
 use lambda_runtime::{Error, LambdaEvent};
 use processor::Processor;
 
-pub async fn handle(event: LambdaEvent<SqsEventObj<S3Event>>) -> Result<(), Error> {
+pub async fn handle(event: LambdaEvent<CloudWatchEvent<S3Event>>) -> Result<(), Error> {
     let processor = &Processor::new()
         .from_env()
         .auto_schema()
         .for_region("us-east-2")
         .await;
 
-    for record in &event.payload.records {
-        for s3_record in &record.body.records {
-            if s3_record.s3.bucket.name.is_none() || s3_record.s3.object.key.is_none() {
-                tracing::error!("Invalid payload - no s3 bucket or key. Skipping");
-            } else {
-                let stream = processor
-                    .s3_client_guard()
-                    .read(
-                        s3_record.s3.bucket.name.as_ref().unwrap().to_string(),
-                        s3_record.s3.object.key.as_ref().unwrap().to_string(),
-                    )
-                    .await;
-                match stream {
-                    Ok(stream) => processor.process(stream).await,
-                    Err(err) => {
-                        tracing::error!(?err, "Stream processing failed.")
-                    }
+    let event = event.payload.detail?;
+    for s3_record in &event.records {
+        if s3_record.s3.bucket.name.is_none() || s3_record.s3.object.key.is_none() {
+            tracing::error!("Invalid payload - no s3 bucket or key. Skipping");
+        } else {
+            let stream = processor
+                .s3_client_guard()
+                .read(
+                    s3_record.s3.bucket.name.as_ref().unwrap().to_string(),
+                    s3_record.s3.object.key.as_ref().unwrap().to_string(),
+                )
+                .await;
+            match stream {
+                Ok(stream) => processor.process(stream).await,
+                Err(err) => {
+                    tracing::error!(?err, "Stream processing failed.")
                 }
             }
         }
